62101b06937588bdb15f823134bdc08b86ea4bb2
streamich <streamich@gmail.com>
2018-11-20 21:44:13 +0000

    feat: 🎸 implement all Array.prototype functions in func way

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,1 +1,96 @@
-xit('Jest working', () => {});
+import {slice, sort, concat, copyWithin} from '..';
+
+describe('slice()', () => {
+  test('returns the same array, when no arguments provided', () => {
+    expect(slice([])).toEqual([]);
+    expect(slice([1, 2, 3])).toEqual([1, 2, 3]);
+  });
+
+  test('returns a copy of the array', () => {
+    const arr = [1, 2, 3];
+    const result = slice(arr);
+    expect(result).toEqual(arr);
+    expect(result).not.toBe(arr);
+  });
+
+  test('does not mutate the original array', () => {
+    const arr = [1, 2, 3];
+    const result = slice(arr);
+    expect(result).toEqual(arr);
+    expect(arr).not.toBe([1, 2, 3]);
+  });
+
+  test('is idempotent', () => {
+    const arr = [1, 2, 3];
+    const result = slice(slice(slice(arr)));
+    expect(result).toEqual(arr);
+  });
+
+  test('can specify slice start', () => {
+    const arr = [1, 2, 3, 4];
+    const result = slice(arr, 1);
+    expect(result).toEqual([2, 3, 4]);
+    expect(arr).toEqual([1, 2, 3, 4]);
+  });
+
+  test('can specify slice end', () => {
+    const arr = [1, 2, 3, 4, 5, 6];
+    const result = slice(arr, 1, 3);
+    expect(result).toEqual([2, 3]);
+    expect(arr).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+});
+
+describe('sort()', () => {
+  test('sorts an array', () => {
+    expect(sort([2, 1, 3])).toEqual([1, 2, 3]);
+  });
+
+  test('does not mutate original array', () => {
+    const arr = [2, 3, 1];
+    sort(arr);
+    expect(arr).toEqual([2, 3, 1]);
+  });
+
+  test('can use comparator function', () => {
+    const comparator = (a, b) => a < b ? 1 : -1;
+    expect(sort([2, 1, 3], comparator)).toEqual([3, 2, 1]);
+  });
+});
+
+describe('concat()', () => {
+  test('concatenates two empty arrays', () => {
+    expect(concat([], [])).toEqual([]);
+  });
+
+  test('concatenates two arrays', () => {
+    const arr1 = [1];
+    const arr2 = [2];
+    const result = concat(arr1, arr2);
+
+    expect(result).toEqual([1, 2]);
+  });
+
+  test('does not mutate original arrays', () => {
+    const arr1 = [1];
+    const arr2 = [2];
+    concat(arr1, arr2);
+
+    expect(arr1).toEqual([1]);
+    expect(arr2).toEqual([2]);
+  });
+});
+
+describe('copyWithin()', () => {
+  test('copies requested part', () => {
+    const arr = [1, 2, 3, 4];
+    const result = copyWithin(arr, 0, 1, 3);
+    expect(result).toEqual([2, 3, 3, 4]);
+  });
+
+  test('does not mutate original array', () => {
+    const arr = [1, 2, 3, 4];
+    copyWithin(arr, 0, 1, 3);
+    expect(arr).toEqual([1, 2, 3, 4]);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,95 @@
-console.log('Hello world!'); // tslint:disable-line no-console
+const prototype = Array.prototype;
+const {
+  slice: $slice,
+  sort: $sort,
+  concat: $concat,
+  copyWithin: $copyWithin,
+  every: $every,
+  join: $join,
+  find: $find,
+  entries: $entries,
+  flat: $flat,
+  flatMap: $flatMap,
+  forEach: $forEach,
+  filter: $filter,
+  map: $map,
+  pop: $pop,
+  push: $push,
+  shift: $shift,
+  reverse: $reverse,
+  some: $some,
+  splice: $splice,
+  unshift: $unshift,
+  values: $values,
+  reduce: $reduce,
+  reduceRight: $reduceRight,
+  includes: $includes,
+  keys: $keys,
+  fill: $fill,
+  findIndex: $findIndex,
+  indexOf: $indexOf,
+  lastIndexOf: $lastIndexOf,
+  toLocaleString: $toLocaleString,
+  toString: $toString,
+} = prototype;
+
+export const slice = <T>(arr: T[], start?: number, end?: number) =>
+  $slice.call(arr, start, end);
+export const sort = <T>(arr: T[], comparator?: (a: T, b: T) => number) =>
+  $sort.call(slice(arr), comparator);
+export const concat = <T>(arr: T[], ...rest: T[][]): T[] =>
+  $concat.apply(arr, rest);
+export const copyWithin = <T>(arr: T[], target: number, start: number, end?: number): T[] =>
+  $copyWithin.call(slice(arr), target, start, end);
+export const every = <T>(arr: T[], callback: (value: never, index: number, array: never[]) => boolean): boolean =>
+  $every.call(arr, callback);
+export const join = (arr, separator) => $join.call(arr, separator);
+export const find = (arr: any[], predicate) => $find.call(arr, predicate);
+export const entries = (arr: any[]) => $entries.call(arr);
+export const flat = (arr, depth?: number) => $flat.call(arr, depth);
+export const flatMap = (arr, callback) => $flatMap.call(arr, callback);
+export const forEach =  (arr, callback) => $forEach.call(arr, callback);
+export const filter = (arr, callback) => $filter.call(arr, callback);
+export const map = (arr, callback) => $map.call(arr, callback);
+export const pop = (arr) => {
+  const copy = slice(arr);
+  return [$pop.call(copy), copy];
+};
+export const push = (arr, ...items) => $push.apply(slice(arr), items);
+export const shift = (arr) => {
+  const copy = slice(arr);
+  return [$shift.call(copy), copy];
+};
+export const reverse = (arr) => $reverse.call(slice(arr));
+export const some = (arr, callback) => $some.call(arr, callback);
+export const splice = (arr, start: number, deleteCount?: number) => {
+  const copy = slice(arr);
+  $splice.call(copy, start, deleteCount);
+  return copy;
+};
+export const unshift = (arr, ...items) => {
+  const copy = slice(arr);
+  $unshift.apply(copy, items);
+  return copy;
+};
+export const values = (arr) => $values.call(arr);
+export const keys = (arr) => $keys.call(arr);
+export const reduce = (arr, callback, initialValue) =>
+  $reduce.call(arr, callback, initialValue);
+export const reduceRight = (arr, callback, initialValue) =>
+  $reduceRight.call(arr, callback, initialValue);
+export const includes = (arr, searchElement, fromIndex) =>
+  $includes.call(arr, searchElement, fromIndex);
+export const fill = (arr, value, start, end) => {
+  const copy = slice(arr);
+  $fill.call(arr, value, start, end);
+  return copy;
+};
+export const findIndex = (arr, predicate) => $findIndex.call(arr, predicate);
+export const indexOf = (arr, searchElement, fromIndex) =>
+  $indexOf.call(arr, searchElement, fromIndex);
+export const lastIndexOf = (arr, searchElement, fromIndex) =>
+  $lastIndexOf.call(arr, searchElement, fromIndex);
+export const length = (arr) => arr.length;
+export const toLocaleString = (arr) => $toLocaleString.call(arr);
+export const toString = (arr) => $toString.call(arr);
